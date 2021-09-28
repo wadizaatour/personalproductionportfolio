f7d4e61eb3622ce3fcd7fe7c61783b761b5d57c2
wadizaatour <zaatourwadi1@gmail.com>
2021-09-29 00:28:37 +0200

    First commit

---
+++ src/views/Landing.vue
@@ -0,0 +1,347 @@
+<template>
+  <div class="wrapper">
+    <parallax class="section page-header header-filter" :style="headerStyle">
+      <div class="container">
+        <div class="md-layout">
+          <div
+            class="md-layout-item md-size-50 md-small-size-70 md-xsmall-size-100"
+          >
+            <h1 class="title">Your Story Starts With Us.</h1>
+            <h4>
+             Wadi is a Tunisian north africain music producer based in Utrecht - Netherlands
+             Being facinated by music from the age of 4 and a bass player and multi-instrumentist for more then 15 yeas.
+             I had the chance to work with different musiciens from all around the world and glad to have this experience to 
+             be able to produce a multi-fusion-culteral music that varies from jazz, blues, reggae, funk, rap, electronic music and of course the North africain gnawa style...
+            </h4>
+            <br />
+            <md-button
+              href="https://www.youtube.com/watch?v=ZvVytobfR_U"
+              class="md-success md-lg"
+              target="_blank"
+              ><i class="fas fa-play"></i> Watch video</md-button
+            >
+          </div>
+        </div>
+      </div>
+    </parallax>
+    <div class="main main-raised">
+      <div class="section">
+        <div class="container">
+          <div class="md-layout">
+            <div
+              class="md-layout-item md-size-66 md-xsmall-size-100 mx-auto text-center"
+            >
+              <h2 class="title text-center">Any creative idea in mind ?</h2>
+              <h5 class="description">
+               If you have any idea we can always have a discussion or a meeting in person or online!
+               Very ooen minded atmosphere that can lead us together to realise our dreams or even more
+              </h5>
+            </div>
+          </div>
+          <div class="features text-center">
+            <div class="md-layout">
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="info">
+                  <div class="icon icon-info">
+                     <img :src="imgIcon1" alt="Circle Image" />
+                  </div>
+                  <h4 class="info-title">Recording Session</h4>
+                  <p>
+                    With assitance for enhancing your ideas by exchanging nice musical moment with some improvisation before we start the session
+                  </p>
+                </div>
+              </div>
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="info">
+                  <div class="icon icon-success">
+                     <div class="icon icon-info">
+                     <img :src="imgIcon2" alt="Circle Image" />
+                  </div>
+                  </div>
+                  <h4 class="info-title">Videography</h4>
+                  <p>
+                    Wiser will be if we pick up a location that suits your song and produce a video clip with the latest editing and recording material 
+                  </p>
+                </div>
+              </div>
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="info">
+                  <div class="icon icon-danger">
+                    <img :src="imgIcon3" alt="Circle Image" />
+                  </div>
+                  <h4 class="info-title">Album Production</h4>
+                  <p>
+                    From one song you can go anywhere! maybe an whole album? of course it's possible, a rented professional studio based on your prefered location can be set and ready for you!
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <carousel></carousel>
+      <div class="section text-center">
+        <div class="container">
+          <h2 class="title">Here is our team</h2>
+          <div class="team">
+            <div class="md-layout">
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="team-player">
+                  <md-card class="md-card-plain">
+                    <div class="md-layout-item md-size-50 mx-auto">
+                      <img
+                        :src="teamImg1"
+                        alt="Thumbnail Image"
+                        class="img-raised rounded-circle img-fluid"
+                      />
+                    </div>
+                    <h4 class="card-title">
+                      Gigi Hadid
+                      <br />
+                      <small class="card-description text-muted">Model</small>
+                    </h4>
+
+                    <md-card-content>
+                      <p class="card-description">
+                        You can write here details about one of your team
+                        members. You can give more details about what they do.
+                        Feel free to add some <a href="#">links</a> for people
+                        to be able to follow them outside the site.
+                      </p>
+                    </md-card-content>
+
+                    <md-card-actions class="text-center">
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-twitter"></i>
+                      </md-button>
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-instagram"></i>
+                      </md-button>
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-facebook-square"></i>
+                      </md-button>
+                    </md-card-actions>
+                  </md-card>
+                </div>
+              </div>
+           
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="team-player">
+                  <md-card class="md-card-plain">
+                    <div class="md-layout-item md-size-50 mx-auto">
+                      <img
+                        :src="teamImg2"
+                        alt="Thumbnail Image"
+                        class="img-raised rounded-circle img-fluid"
+                      />
+                    </div>
+                    <h4 class="card-title">
+                      Carla Hortensia
+                      <br />
+                      <small class="card-description text-muted"
+                        >Designer</small
+                      >
+                    </h4>
+
+                    <md-card-content>
+                      <p class="card-description">
+                        You can write here details about one of your team
+                        members. You can give more details about what they do.
+                        Feel free to add some <a href="#">links</a> for people
+                        to be able to follow them outside the site.
+                      </p>
+                    </md-card-content>
+
+                    <md-card-actions class="text-center">
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-twitter"></i>
+                      </md-button>
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-linkedin"></i>
+                      </md-button>
+                    </md-card-actions>
+                  </md-card>
+                </div>
+              </div>
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="team-player">
+                  <md-card class="md-card-plain">
+                    <div class="md-layout-item md-size-50 mx-auto">
+                      <img
+                        :src="teamImg3"
+                        alt="Thumbnail Image"
+                        class="img-raised rounded-circle img-fluid"
+                      />
+                    </div>
+                    <h4 class="card-title">
+                      Kendall Jenner
+                      <br />
+                      <small class="card-description text-muted">Model</small>
+                    </h4>
+
+                    <md-card-content>
+                      <p class="card-description">
+                        You can write here details about one of your team
+                        members. You can give more details about what they do.
+                        Feel free to add some <a href="#">links</a> for people
+                        to be able to follow them outside the site.
+                      </p>
+                    </md-card-content>
+
+                    <md-card-actions class="text-center">
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-twitter"></i>
+                      </md-button>
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-instagram"></i>
+                      </md-button>
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-facebook-square"></i>
+                      </md-button>
+                    </md-card-actions>
+                  </md-card>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section section-contacts">
+        <div class="container">
+          <div class="md-layout">
+            <div class="md-layout-item md-size-66 md-xsmall-size-100 mx-auto">
+              <h2 class="text-center title">Work with us</h2>
+              <h4 class="text-center description">
+                Divide details about your product or agency work into parts.
+                Write a few lines about each one and contact us about any
+                further collaboration. We will responde get back to you in a
+                couple of hours.
+              </h4>
+              <form class="contact-form">
+                <div class="md-layout">
+                  <div class="md-layout-item md-size-50">
+                    <md-field>
+                      <label>Your Name</label>
+                      <md-input v-model="name" type="text"></md-input>
+                    </md-field>
+                  </div>
+                  <div class="md-layout-item md-size-50">
+                    <md-field>
+                      <label>Your Email</label>
+                      <md-input v-model="email" type="email"></md-input>
+                    </md-field>
+                  </div>
+                </div>
+                <md-field maxlength="5">
+                  <label>Your Message</label>
+                  <md-textarea v-model="message"></md-textarea>
+                </md-field>
+                <div class="md-layout">
+                  <div class="md-layout-item md-size-33 mx-auto text-center">
+                    <md-button class="md-success">Send Message</md-button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Carousel from "./components/Carousel.vue";
+export default {
+  bodyClass: "landing-page",
+  props: {
+    header: {
+      type: String,
+      default: require("@/assets/img/bg-music.jpg")
+    },
+    teamImg1: {
+      type: String,
+      default: require("@/assets/img/faces/avatar.jpg")
+    },
+    teamImg2: {
+      type: String,
+      default: require("@/assets/img/faces/christian.jpg")
+    },
+    teamImg3: {
+      type: String,
+      default: require("@/assets/img/faces/kendall.jpg")
+    },
+    imgIcon1: {
+      type: String,
+      default: require("@/assets/img/recording-icon.jpg")
+    },
+    imgIcon2: {
+      type: String,
+      default: require("@/assets/img/video-icon.jpg")
+    },
+    imgIcon3: {
+      type: String,
+      default: require("@/assets/img/all-icon.jpg")
+    },
+  },
+  components: {
+    Carousel
+  },
+  data() {
+    return {
+      name: null,
+      email: null,
+      message: null
+    };
+  },
+  computed: {
+    headerStyle() {
+      return {
+        backgroundImage: `url(${this.header})`
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.md-card-actions.text-center {
+  display: flex;
+  justify-content: center !important;
+}
+.contact-form {
+  margin-top: 30px;
+}
+
+.md-has-textarea + .md-layout {
+  margin-top: 15px;
+}
+.md-has-textarea + .md-layout {
+  margin-top: 15px;
+}
+ 
+</style>
---
+++ src/views/components/Carousel.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+      <div class="md-layout">
+        <div class="md-layout-item md-size-66 mx-auto md-small-size-100">
+          <md-card>
+            <carousel
+              :per-page="1"
+              loop
+              :speed="700"
+              autoplay
+              :autoplay-timeout="5000"
+              :mouse-drag="false"
+              navigationEnabled
+              navigationNextLabel="<i class='material-icons'>keyboard_arrow_right</i>"
+              navigationPrevLabel="<i class='material-icons'>keyboard_arrow_left</i>"
+            >
+              <slide>
+                <div class="carousel-caption">
+                  <h4>
+                    <md-icon>location_on</md-icon>
+                    Somewhere Beyond, United States
+                  </h4>
+                </div>
+                <img :src="carousel1" alt="carousel1" />
+              </slide>
+              <slide>
+                <div class="carousel-caption">
+                  <h4>
+                    <md-icon>location_on</md-icon>
+                    Yellowstone National Park, United States
+                  </h4>
+                </div>
+                <img :src="carousel2" alt="carousel2" />
+              </slide>
+            </carousel>
+          </md-card>
+        </div>
+      </div>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    data(){
+        return {
+            carousel1: require("@/assets/img/wadi-image1.jpg"),
+            carousel2: require("@/assets/img/wadi-image2.jpg"),
+            carousel3: require("@/assets/img/wadi-image3.jpg")
+        }
+    }
+})
+</script>
